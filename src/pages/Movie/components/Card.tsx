7f4007ed9aa5cda170fae190653e017d580affeb
Gabriel Hermenegildo <gabriel.f.hermenegildo@gmail.com>
2025-01-29 21:02:58 -0300

    style: página de filmes responsiva

--- src/pages/Movie/components/Card.tsx
+++ src/pages/Movie/components/Card.tsx
@@ -12,11 +12,11 @@ const Card = ({ title, value, className, variant = 'primary' }: CardProps) => {
   const isGenre = value instanceof Array
 
   return (
-    <div className={`bg-mauve3/75 py-3 px-3 rounded-sm flex-1 backdrop-filter backdrop-blur-[2px] ${className}`}>
-      <h3 className="text-mauve11 font-bold text-sm uppercase">{title}</h3>
+    <div className={`bg-mauve3/75 py-3 px-3 xl:px-4 xl:py-4 h-fit rounded-sm flex-1 backdrop-filter backdrop-blur-[2px] ${className}`}>
+      <h3 className="text-mauve11 font-bold text-sm uppercase xl:text-base">{title}</h3>
       {!isGenre ? (
         <span
-          className={`text-sm text-mauve12 ${variant === 'secondary' ?
+          className={`text-sm xl:text-base text-mauve12 ${variant === 'secondary' ?
             'font-semibold  text-left' : 'text-mauve12 font-normal'}
         `}>
           {value}
@@ -24,7 +24,7 @@ const Card = ({ title, value, className, variant = 'primary' }: CardProps) => {
       ) : (
         <div className='flex flex-wrap gap-2 mt-2'>
           {value.map((genre, index) => (
-            <Badge key={index} tabIndex={0} className="bg-purplea3 text-mauve12 text-xs font-semibold rounded-sm uppercase">
+            <Badge key={index} tabIndex={0} className="bg-purplea3 text-mauve12 text-xs xl:text-sm font-semibold rounded-sm uppercase">
               {genre.name}
             </Badge>
           ))}
