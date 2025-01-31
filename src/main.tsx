889886f45b7fbcd50def91a66b9b3878fc6a797a
Gabriel Hermenegildo <gabriel.f.hermenegildo@gmail.com>
2025-01-30 21:31:07 -0300

    fix: import bugado

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import '@/index.css'
 import App from '@/App.tsx'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router";
-import { FilterProvider } from './../../cubos-movies/src/contexts/Filters';
+import { FilterProvider } from './contexts/Filters';
 
 const queryClient = new QueryClient();
 
